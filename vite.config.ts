5d9db2f0bc9177d79cb3731ed67f6d091740c210
komu <sahukomendra721@gmail.com>
2025-12-01 20:47:34 +0530

    light changes second attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-// export default defineConfig(({ mode }) => ({
-//   base: mode === "production" ? "/my-portfolio/" : "/",
-//   // base: "",   // FIXED for local dev
-
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
-
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/my-portfolio/",
   plugins: [react()],
