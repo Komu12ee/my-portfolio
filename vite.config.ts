043e8cc0c510393f20475611d3f04071c12e623a
komu <sahukomendra721@gmail.com>
2025-12-01 20:40:46 +0530

    little bit changes regarding tdeploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/my-portfolio/" : "/",
-  // base: "",   // FIXED for local dev
+// export default defineConfig(({ mode }) => ({
+//   base: mode === "production" ? "/my-portfolio/" : "/",
+//   // base: "",   // FIXED for local dev
 
-  server: {
-    host: "::",
-    port: 8080,
-  },
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+export default defineConfig({
+  base: "/my-portfolio/",
+  plugins: [react()],
+});
