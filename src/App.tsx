22e58f3d3415be4730e988ab59cfdfa7050a7709
komu <sahukomendra721@gmail.com>
2026-01-07 21:40:16 +0530

    issue in Toster.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Toaster from "@/components/ui/toaster"
-import Button from "@/components/ui/button"
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as ShadcnToaster } from "@/components/ui/Toaster";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import Blogs from "./pages/Blogs";
@@ -19,8 +17,10 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      {/* Toast Providers */}
+      <ShadcnToaster />
+      <SonnerToaster />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -29,7 +29,6 @@ const App = () => (
           <Route path="/resume" element={<Resume />} />
           <Route path="/publication" element={<Publication />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
